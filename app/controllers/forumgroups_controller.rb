ad3c67f924ed82fca404c26ed3491ec4320fd4ba
jomo <github@jomo.tv>
2013-08-02 08:14:32 +0200

    wrong order fix

--- app/controllers/forumgroups_controller.rb
+++ app/controllers/forumgroups_controller.rb
@@ -1,6 +1,6 @@
 class ForumgroupsController < ApplicationController
 
   def index
-    @groups = Forumgroup.all.sort{|s| s[:position]}
+    @groups = Forumgroup.all.sort_by{|s| s[:position]}
   end
 end
