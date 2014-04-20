c0a57f1554921d05e405fe1b5ac7d791634882e8
jomo <github@jomo.tv>
2014-04-20 03:34:29 +0200

    fuck it

--- db/migrate/09_create_register_tokens.rb
+++ db/migrate/09_create_register_tokens.rb
@@ -1,10 +1,9 @@
 class CreateRegisterTokens < ActiveRecord::Migration
   def change
-     create_table :register_tokens, id: false do |t|
+     create_table :register_tokens do |t|
        t.string :uuid, limit: 32, unique: true, primary: true, null: false
        t.string :token, limit: 6, null: false
        t.string :email, unique: true, null: false
      end
-     add_index :register_tokens, :uuid, :string
   end
 end
