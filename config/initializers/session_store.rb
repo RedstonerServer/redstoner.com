2e687216ee688082026f5fb2e573823609b75f17
jomo <github@jomo.tv>
2014-05-02 01:50:12 +0200

    change back to active_record_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,6 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Redstoner::Application.config.session_store :active_record_store
-
-Redstoner::Application.config.session_store :cookie_store,
+Redstoner::Application.config.session_store :active_record_store,
   key: 'redstoner_session',
   expire_after: 5.days
