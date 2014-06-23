498aa0360630eb8fcd562f5fb3b910828577db34
jomo <github@jomo.tv>
2014-06-23 19:20:40 +0200

    adjust new path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/redstoner/website/public"
+root = "/var/rails_site/"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT="${TIMEOUT-60}"
-APP_ROOT="/home/redstoner/website/public"
+APP_ROOT="/var/rails_site/"
 PID="$APP_ROOT/tmp/pids/unicorn.pid"
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER="www-data"
