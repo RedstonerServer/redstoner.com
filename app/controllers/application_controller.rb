0f9baa23a66e00dd8cbd0f94e960ae10ce9db54a
jomo <github@jomo.tv>
2014-06-24 00:09:48 +0200

    remove password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :update_ip, :update_seen, :check_banned
   # TODO: force_ssl
 
-  # TODO: remove this!
-  http_basic_authenticate_with name: "redstone", password: "sheep_"
 
   helper :all
   include UsersHelper
