6769982fe337f310208efbb58a0f1dc37c0fe584
jomo <github@jomo.tv>
2014-06-25 00:05:19 +0200

    fixes for production

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,7 +12,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT="${TIMEOUT-60}"
-APP_ROOT="/home/www-data/apps/redstoner"
+APP_ROOT="/home/www-data/apps/redstoner/current"
 PID="$APP_ROOT/tmp/pids/unicorn.pid"
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER="www-data"
