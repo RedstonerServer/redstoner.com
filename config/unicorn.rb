81518f1ec4fa64dcb7d92f16e608e5c196caa452
jomo <github@jomo.tv>
2014-06-25 00:08:54 +0200

    fixes for production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/www-data/apps/redstoner"
+root = "/home/www-data/apps/redstoner/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
