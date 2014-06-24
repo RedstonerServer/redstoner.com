e24e7b3460e103dff0d1a4ef4f1378432bb9a304
jomo <github@jomo.tv>
2014-06-24 23:39:57 +0200

    removed more crap

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,11 +33,6 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
-  task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  after "deploy:finalize_update", "deploy:symlink_config"
-
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
