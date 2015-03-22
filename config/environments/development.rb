976799b9962295a6c4b0459f32b19b26fbebffeb
jomo <github@jomo.tv>
2015-03-22 23:16:57 +0100

    add developent.rb & production.rb

---
+++ config/environments/development.rb
@@ -0,0 +1,46 @@
+Redstoner::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Print deprecation notices to the logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+  config.eager_load = false
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = {
+    host: 'local.redstoner.com',
+    port: 3000
+  }
+
+  config.action_mailer.smtp_settings = {
+    address:        "smtp.gmail.com",
+    port:           587,
+    domain:         "google.com",
+    authentication: "plain",
+    user_name:      "redstonerserver@gmail.com",
+    password:       ENV["GMAIL_PASSWORD"],
+  }
+
+end
