f507b77baa33831b45fee89c64d21848de629492
jomo <github@jomo.tv>
2015-03-23 22:32:47 +0100

    deploy from :octocat:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :repo_url, 'git@bitbucket.org:redstonesheep/redstoner.git'
+set :repo_url, 'git@github.com:RedstonerServer/redstoner.com.git'
 
 set :scm, :git
 
