ceaec53d39b6a51135ece76d0ffdc098a3058e08
jomo <github@jomo.tv>
2016-01-29 23:57:44 +0100

    remove mysql 5.6 fix

--- config/initializers/mysql_modern_column_type_defaults.rb
+++ config/initializers/mysql_modern_column_type_defaults.rb
@@ -4,12 +4,6 @@ ActiveRecord::ConnectionAdapters::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES.ta
   # Decrease default string length from 255 -> 191 chars so *_type columns
   # can be indexed with utf8mb4 without blowing 767 byte max key length.
   defaults[:string][:limit] = 191
-
-  # Use microsecond precision for all timestamps. High-precision times are
-  # first available in MySQL 5.6, but default to second precision for
-  # backward compatibility.
-  defaults[:datetime][:limit] = 6
-  defaults[:time][:limit] = 6
 end
 
 
