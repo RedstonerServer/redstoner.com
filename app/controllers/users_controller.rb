db1c10eb9b89900db2e1f1c48a8d76cf669ab277
Jonas Folvik <Jonas-folvik@hotmail.com>
2016-06-12 20:36:53 +0200

    An Option to resend the confirmation mail
    You should now be able to resend the confirmation mail with the click of
    a link that is next to the warning that the mail isn't confirmed.
    
    Resend the confirmation mail cleaning
    
    Just a way to re use the files and stuff we already have instead of the
    file I created even though I could use the register_mail we have.
    
    Change of route and link to button
    
    I changed the route from GET to POST because of security reasons, and
    changed the link_to to a button_to
    
    changed the notice
    
    I changed the notice so it said check for the mail instead of check for
    the link
    
    Changed notice and button
    
    Changed the notice to say "Check your inbox" instead of "Check your
    mail" also changed the way the class looks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -138,6 +138,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def resend_mail
+    RedstonerMailer.register_mail(@user, false).deliver_now
+    flash[:notice] = "Check your inbox for the confirmation mail."
+    redirect_to users_path(@user)
+  end
+
   def update
     if (mod? && current_user.role >= @user.role ) || (@user.is?(current_user) && confirmed?)
       if mod?
