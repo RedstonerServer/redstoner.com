882b1512ca214f3ebae5ac822c0d7e020d345d67
MrYummy <elemental428@gmail.com>
2017-05-13 23:53:23 +0200

    Added Who's Playing? screen

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,8 @@ Redstoner::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_controller.perform_caching = true
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
