276949c0cd800794118ae0ba74d3e7d09edb2a76
MrYummy <elemental428@gmail.com>
2017-05-28 17:39:06 -0400

    Added badge system

---
+++ app/models/badge.rb
@@ -0,0 +1,18 @@
+class Badge < ActiveRecord::Base
+  include Comparable
+  has_many :users
+
+  def self.get (input)
+    if input.is_a?(String) || input.is_a?(Symbol)
+      Badge.find_by_name(input)
+    elsif input.is_a?(Fixnum)
+      Badge.find_by_id(input)
+    elsif input.is_a?(Badge)
+      return input
+    end
+  end
+
+  def to_s
+    self.name
+  end
+end
