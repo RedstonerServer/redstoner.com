e7463524af03a62b8ba4adbdee29efe5f547c2d4
MrYummy <elemental428@gmail.com>
2017-05-28 17:46:54 -0400

    Added Who's Playing? screen

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,7 @@
 class StaticsController < ApplicationController
 
+  caches_action :online, expires_in: 10.seconds
+
   def index
     if current_user
       redirect_to blogposts_path
@@ -14,4 +16,8 @@ class StaticsController < ApplicationController
   def donate
   end
 
+  def online
+    @players = JSON.parse(File.read("/etc/minecraft/redstoner/plugins/JavaUtils/players.json"))["players"]
+    @players.collect!{ |p| User.find_by(uuid: p["uuid"].tr("-", "")) or User.new(name: p["name"], ign: p["name"], uuid: p["uuid"].tr("-", ""), role: Role.get("normal"), confirmed: true) }.sort_by!(&:role).reverse!
+  end
 end
