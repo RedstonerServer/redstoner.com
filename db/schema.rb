91d6082d3759bd65a95a0dee063a46d2e6f6d2f6
Logan Fick <logaldeveloper@protonmail.com>
2017-10-18 17:06:41 -0400

    Fixed schema version number.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703003647) do
+ActiveRecord::Schema.define(version: 20171013001146) do
 
   create_table "badges", force: :cascade do |t|
     t.string  "name",   limit: 191
