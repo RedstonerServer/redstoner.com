0de6b08b4e4d90025ce7ba92e7cb1fe3e5abfe47
Logan Fick <logaldeveloper@protonmail.com>
2018-09-13 20:48:24 -0400

    Removed hostnames from user profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,6 @@ class UsersController < ApplicationController
       flash.now[:alert] = "An error occured while checking if this user is banned from the server!"
       @ban_json = nil
     end
-
-    begin
-      @hostname = Resolv.new.getname(@user.last_ip)
-    rescue
-      @hostname = "No hostname found."
-    end
   end
 
   # SIGNUP
