f6cb866b854f661152601d195901360b73aeb8eb
Futseh <jonas.folvik@gmail.com>
2019-02-24 18:17:46 +0100

    Can disable the oppurtunity to delete threads in the forums

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -89,7 +89,7 @@ class ForumsController < ApplicationController
   end
 
   def forum_params(add = [])
-    a = [:name, :position, :role_read_id, :role_write_id, :necro_length] + add
+    a = [:name, :position, :role_read_id, :role_write_id, :necro_length, :disable_deletion] + add
     params.require(:forum).permit(a)
   end
 end
